4614b0bca72c4488b99db18d3296c3af3c8c42f9
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2023-12-11 13:07:34 -0300

    Tentando fazer animação funcionar

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jump = 5f;
+    [SerializeField] private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject child = transform.GetChild(0).gameObject;
+        animator = child.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,6 +19,14 @@ public class PlayerController : MonoBehaviour
     {
         float dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float dz = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        if(dx != 0 || dz != 0)
+        {
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
         transform.position += new Vector3(dx, 0, dz);
 
         if (Input.GetKeyDown(KeyCode.Space))
