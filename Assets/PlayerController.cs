c1089f98fc917309a0da0399855488c0eefb2964
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2023-12-13 12:36:06 -0300

    Tentando fazer esqueleto funcionar

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 5f;
     private int jumpCount = 0;
+    private bool isGrounded = true;
 
     [Header("Camera settings")]
     [SerializeField] private float eyeSpeed = 5f;
@@ -48,7 +49,7 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Moving", false);
             animator.SetFloat("Velocity", 0);
         }
-        transform.Translate(velocity * Time.deltaTime, Space.Self);;  
+        transform.Translate(velocity * Time.deltaTime, Space.Self);
         mouseH += Input.GetAxis("Mouse X");
         
         Quaternion rotY;
@@ -60,13 +61,25 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount<1)
         {
             GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            animator.SetTrigger("Jump");
+            animator.SetInteger("Jumping", 1);
+            animator.SetInteger("Trigger Number", 1);
+            animator.SetTrigger("Trigger");
             jumpCount++;
+            isGrounded = false;
         }
-        if(transform.position.y < 0.01)
+        else if(!isGrounded && GetComponent<Rigidbody>().velocity.y < 0 && transform.position.y <=0.1f)
         {
             jumpCount = 0;
+            animator.SetInteger("Jumping", 0);
+            animator.SetInteger("Trigger Number", 1);
+            animator.SetTrigger("Trigger");
+            isGrounded = true;
         }
-        
+        else if(!isGrounded && GetComponent<Rigidbody>().velocity.y < 0)
+        {
+            animator.SetInteger("Jumping", 2);
+            animator.SetInteger("Trigger Number", 1);
+            animator.SetTrigger("Trigger");
+        }
     }
 }
